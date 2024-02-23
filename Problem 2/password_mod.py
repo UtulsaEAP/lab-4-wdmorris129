1cbe8e574a1051dac4fca8fc0061a8f0c58a2db5
Will Morris <wdm9727@utulsa.edu>
2024-02-23 15:08:40 -0600

    added name to problem 2

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,14 +1,15 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Will Morris
+Lab Time: Friday Afternoon
 """
 
 def password_mod():
     word = input()
     password = ''
     # Type your code here.
+    while 
 
 if __name__ == "__main__":
     password_mod()
