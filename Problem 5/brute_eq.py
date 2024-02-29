94e24aaebb3ca53b2f0042195cd0c16742181b79
Will Morris <wdm9727@utulsa.edu>
2024-02-28 19:45:43 -0600

    added code for problem 5

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -7,23 +7,49 @@ Name:
 Lab Time:
 """
 
-def brute_eq():
-    #Read in first equation, ax + by = c 
-    a = int(input())
-    b = int(input())
-    c = int(input())
+# def brute_eq():
+#     #Read in first equation, ax + by = c 
+#     a = int(input())
+#     b = int(input())
+#     c = int(input())
 
-    #Read in second equation, dx + ey = f
-    d = int(input())
-    e = int(input())
-    f = int(input())
+#     #Read in second equation, dx + ey = f
+#     d = int(input())
+#     e = int(input())
+#     f = int(input())
 
-    # YOUR CODE HERE
-    for x in range [-10,10]:
-        for y in range [-10,10]:
-            if x in 
-
-
+#     # YOUR CODE HERE
+#     for x in range [-10,10]:
+#         for y in range [-10,10]:
+#             if a * x + b * y == c and d * x + e * y == f:
+#                 return x , y
+#         return None
     
-if __name__ == "__main__":
-    brute_eq()
+# if __name__ == "__main__":
+#     brute_eq()
+
+def find_integer_solution(a1, b1, c1, a2, b2, c2):
+    for x in range(-10, 11):
+        for y in range(-10, 11):
+            if a1 * x + b1 * y == c1 and a2 * x + b2 * y == c2:
+                return x, y
+
+    return None
+
+if __name__ == '__main__':
+    # Get coefficients from the user
+    a1 = int(input("Enter coefficient a1: "))
+    b1 = int(input("Enter coefficient b1: "))
+    c1 = int(input("Enter coefficient c1: "))
+    a2 = int(input("Enter coefficient a2: "))
+    b2 = int(input("Enter coefficient b2: "))
+    c2 = int(input("Enter coefficient c2: "))
+
+    # Find the integer solution using brute force
+    solution = find_integer_solution(a1, b1, c1, a2, b2, c2)
+
+    # Output the result
+    if solution:
+        print(f"x = {solution[0]}, y = {solution[1]}")
+    else:
+        print("There is no solution")
