695a63caf0cfc81286ce6c2bcbcf608530f10bd5
Will Morris <wdm9727@utulsa.edu>
2024-02-28 20:20:35 -0600

    edited problem formatting

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -6,27 +6,6 @@ If there is no solution, print "There is no solution".
 Name:
 Lab Time:
 """
-
-# def brute_eq():
-#     a = int(input())
-#     b = int(input())
-#     c = int(input())
-#     d = int(input())
-#     e = int(input())
-#     f = int(input())
-
-#     # YOUR CODE HERE
-#     for x in range [-10,10]:
-#         for y in range [-10,10]:
-#             if a * x + b * y == c and d * x + e * y == f:
-#                 print(f"x = {x}, y = {y}")
-#                 return        
-    
-# print("There is no solution")
-    
-# if __name__ == "__main__":
-#     brute_eq()
-
 def brute_eq():
     a = int(input())
     b = int(input())
