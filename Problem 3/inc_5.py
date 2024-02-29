7662ae4d48af34a59b2ab3fb8d6b563f46815536
Will Morris <wdm9727@utulsa.edu>
2024-02-28 18:45:13 -0600

    Added code for problem 3

--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -6,11 +6,19 @@ Lab Time:
 """
 
 def inc_5():
-    # Write your code here
-    
+    num1 = int(input())
+    num2 = int(input())
+
+    if num2 < num1:
+        print("Second integer can't be less than the first.")
+    else:
+        while num1 < num2:
+            print(num1, end=" ")
+            num1 += 5
+        print()
 
 
 
 
-    if __name__ == '__main__':
-        inc_5()
+if __name__ == '__main__':
+    inc_5()
