3415490852f3b68b3ca6ea423aa11d8a5c1e69d0
Will Morris <wdm9727@utulsa.edu>
2024-02-28 18:46:43 -0600

    changed > to >=

--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -12,7 +12,7 @@ def inc_5():
     if num2 < num1:
         print("Second integer can't be less than the first.")
     else:
-        while num1 < num2:
+        while num1 <=  num2:
             print(num1, end=" ")
             num1 += 5
         print()
