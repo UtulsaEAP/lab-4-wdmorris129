07c14c75dcb579d8e430076f7d3b8df4843c558c
Will Morris <wdm9727@utulsa.edu>
2024-02-28 20:02:24 -0600

    edited problem 5 formatting

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -8,12 +8,9 @@ Lab Time:
 """
 
 # def brute_eq():
-#     #Read in first equation, ax + by = c 
 #     a = int(input())
 #     b = int(input())
 #     c = int(input())
-
-#     #Read in second equation, dx + ey = f
 #     d = int(input())
 #     e = int(input())
 #     f = int(input())
@@ -22,21 +19,15 @@ Lab Time:
 #     for x in range [-10,10]:
 #         for y in range [-10,10]:
 #             if a * x + b * y == c and d * x + e * y == f:
-#                 return x , y
-#         return None
+#                 print(f"x = {x}, y = {y}")
+#                 return        
+    
+# print("There is no solution")
     
 # if __name__ == "__main__":
 #     brute_eq()
 
-def find_integer_solution(a1, b1, c1, a2, b2, c2):
-    for x in range(-10, 11):
-        for y in range(-10, 11):
-            if a1 * x + b1 * y == c1 and a2 * x + b2 * y == c2:
-                return x, y
-
-    return None
-
-if __name__ == '__main__':
+def brute_eq():
     # Get coefficients from the user
     a1 = int(input("Enter coefficient a1: "))
     b1 = int(input("Enter coefficient b1: "))
@@ -46,10 +37,14 @@ if __name__ == '__main__':
     c2 = int(input("Enter coefficient c2: "))
 
     # Find the integer solution using brute force
-    solution = find_integer_solution(a1, b1, c1, a2, b2, c2)
+    for x in range(-10, 11):
+        for y in range(-10, 11):
+            if a1 * x + b1 * y == c1 and a2 * x + b2 * y == c2:
+                print(f"x = {x}, y = {y}")
+                return
 
-    # Output the result
-    if solution:
-        print(f"x = {solution[0]}, y = {solution[1]}")
-    else:
-        print("There is no solution")
+    print("There is no solution")
+
+if __name__ == "__main__":
+    brute_eq()
+
