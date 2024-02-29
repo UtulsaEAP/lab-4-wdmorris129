3519d2ae0b6a1cc9cf3a426fd50a63c7ac407569
Will Morris <wdm9727@utulsa.edu>
2024-02-28 19:09:53 -0600

    edited prblem 4 code

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -6,16 +6,33 @@ Lab Time:
 """
 
 def reverse_string():
-    # YOUR CODE HERE
+# YOUR CODE HERE
     while True:
-        forwardLine = input()
-        if ["Done","done",'d'] in forwardLine:
-            break
-    backwardLine = forwardLine [::-1]
-
-print(backwardLine)
+        forwardLine = input("")
+        
+        if  forwardLine.lower() in ['done', 'd']:
+            break      
+    
+        backwardLine = forwardLine[::-1]
+        print(backwardLine)
     
 
 
 if __name__ == "__main__":
     reverse_string()
+
+# def reverse_input():
+#     while True:
+#         # Get input from the user
+#         line = input("Enter a line of text (or 'Done' to exit): ")
+
+#         # Check if the user wants to exit
+#         if line.lower() in ['done', 'd']:
+#             break
+
+#         # Print the reversed line of text
+#         reversed_line = line[::-1]
+#         print(reversed_line)
+
+# if __name__ == '__main__':
+#     reverse_input()
