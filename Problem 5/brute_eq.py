960a5a218ca911bba01ff3ad6c8450adbec0b66e
Will Morris <wdm9727@utulsa.edu>
2024-03-01 12:13:31 -0600

    trying an older version to see if it works better

--- Problem 5/brute_eq.py
+++ Problem 5/brute_eq.py
@@ -13,15 +13,11 @@ def brute_eq():
     d = int(input())
     e = int(input())
     f = int(input())
-
     for x in range(-10, 11):
         for y in range(-10, 11):
             if a * x + b * y == c and d * x + e * y == f:
                 print(f"x = {x}, y = {y}")
                 return
-
-print("There is no solution")
-
+    print("There is no solution")
 if __name__ == "__main__":
     brute_eq()
-
